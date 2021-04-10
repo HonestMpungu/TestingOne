e9800466530a5a327863feb3b893bf31f5b1926e
Honest Mpungu <mpunguhonest@gmail.com>
2021-04-10 20:23:12 +0200

    commit second changes

--- src/za/ac/cput/testinggit/Trial.java
+++ src/za/ac/cput/testinggit/Trial.java
@@ -2,7 +2,14 @@ package za.ac.cput.testinggit;
 
 public class Trial {
 
+    /**
+     * Honest Mpungu
+     * @param str
+     * @return
+     */
+
     public int lengthOfString(String str) {
+        if(str==null || str.isEmpty()) return 0;
         return str.length();
     }
 }
